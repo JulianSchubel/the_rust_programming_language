cdef9ab5697c697480c5afd5fd4e6d299ad91695
Julian Schubel <schubeljulian@gmail.com>
2023-02-11 12:28:14 +0200

    pig-lating converter

---
+++ chapter_8_common_collections/exercises/pig_latin/target/debug/deps/pig_latin-ccee153fe7e5a124.d
@@ -0,0 +1,5 @@
+/home/js/rust/rust_programming_language/chapter_8_common_collections/exercises/pig_latin/target/debug/deps/pig_latin-ccee153fe7e5a124.rmeta: src/main.rs
+
+/home/js/rust/rust_programming_language/chapter_8_common_collections/exercises/pig_latin/target/debug/deps/pig_latin-ccee153fe7e5a124.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_8_common_collections/exercises/pig_latin/target/debug/pig_latin.d
@@ -0,0 +1,1 @@
+/home/js/rust/rust_programming_language/chapter_8_common_collections/exercises/pig_latin/target/debug/pig_latin: /home/js/rust/rust_programming_language/chapter_8_common_collections/exercises/pig_latin/src/main.rs
